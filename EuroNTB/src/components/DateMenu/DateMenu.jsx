77026fb2379eb33d8b373f27fef403d65d06516d
Haavaala <haavaala@stud.ntnu.no>
2024-06-23 19:34:38 +0200

    Refactor CSS classes for DateMenu component

--- EuroNTB/src/components/DateMenu/DateMenu.jsx
+++ EuroNTB/src/components/DateMenu/DateMenu.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./DateMenu.css";
 
 function DateMenu({ dates, selectedDate, setSelectedDate }) {
+  useEffect(() => {
+    // Set the default selected date to the current date if no date is selected
+    if (!selectedDate) {
+      const today = new Date().toDateString();
+      setSelectedDate(today);
+    }
+  }, [selectedDate, setSelectedDate]);
+
   return (
     <div className="date-menu">
       {/* <h2>Select Date:</h2> */}
       <div className="theDates">
         {dates.map((date) => (
           <button
+            aria-label={date}
             key={date}
             onClick={() => setSelectedDate(date)}
             className={selectedDate === date ? "active" : ""}
--- EuroNTB/src/components/Groups/GroupF.jsx
+++ EuroNTB/src/components/Groups/GroupF.jsx
@@ -1,7 +1,15 @@
-import { MapPin, Users, Tv } from "lucide-react";
+import { useEffect, useState } from "react";
+import { MapPin, Users, Tv, Timer } from "lucide-react";
 import "./Groups.css";
 
 function GroupF({ matches, selectedDate }) {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => setCurrentTime(new Date()), 60000); // Update every minute
+    return () => clearInterval(timer);
+  }, []);
+
   // Filter matches by selected date
   const filteredMatches = selectedDate
     ? matches.filter(
@@ -15,72 +23,103 @@ function GroupF({ matches, selectedDate }) {
         {filteredMatches.length > 0 ? <h1>Group F</h1> : <p></p>}
 
         {filteredMatches.length > 0 ? (
-          filteredMatches.map((match) => (
-            <div className="oneMatch" key={match.id}>
-              <div className="matchHeader">
-                <p>{new Date(match.timestamp).toLocaleString()}</p>
-                <p>Round: {match.round}</p>
-              </div>
-              <div className="wholeMatch">
-                <div className="teams">
-                  <div className="homeTeam">
-                    <img
-                      className="logo"
-                      src={match.homeTeam?.logo.url}
-                      alt={match.homeTeam?.name}
-                    />
-                    <p>{match.homeTeam?.name}</p>
-                  </div>
-                  <p className="result">
-                    {match.result.homeScore90} - {match.result.awayScore90}
-                    <div className="break">
-                      ({match.result.homeScore45} - {match.result.awayScore45})
-                    </div>
+          filteredMatches.map((match) => {
+            // Check if match is ongoing based on current time
+            const matchTime = new Date(match.timestamp);
+            const isEnded = match.liveFeeds?.some((feed) => feed.ended);
+            const isOngoing =
+              !isEnded &&
+              currentTime >= matchTime &&
+              currentTime <= new Date(matchTime.getTime() + 90 * 60000);
+
+            return (
+              <div
+                className={`oneMatch ${
+                  isEnded ? "ended" : isOngoing ? "ongoing" : ""
+                }`}
+                key={match.id}
+              >
+                {/* Header section of the scoreboard */}
+                <div className="matchHeader">
+                  <p className="when">
+                    <Timer />
+                    {isEnded
+                      ? "Finished"
+                      : isOngoing
+                      ? "Ongoing"
+                      : matchTime.toLocaleString()}
                   </p>
-                  <div className="awayTeam">
-                    <img
-                      className="logo"
-                      src={match.awayTeam?.logo.url}
-                      alt={match.awayTeam?.name}
-                    />
-                    <p>{match.awayTeam?.name}</p>
-                  </div>
+                  <p>Round: {match.round}</p>
                 </div>
-                <hr />
-                <div className="matchInfo">
-                  <div className="stadiumInfo">
-                    <div className="stadium">
-                      <MapPin />
-                      <p>{match.stadium?.name}</p>
+                {/* Information about the whole game */}
+                <div className="wholeMatch">
+                  <div className="teams">
+                    <div className="homeTeam">
+                      <img
+                        className="logo"
+                        src={match.homeTeam?.logo.url}
+                        alt={match.homeTeam?.name}
+                      />
+                      <p>{match.homeTeam?.name}</p>
                     </div>
-                    <div className="attendance">
-                      <Users size={24} />
-                      <p>{match.attendance}</p>
+                    <p className="result">
+                      {match.result.homeScore90} - {match.result.awayScore90}
+                      <div className="break">
+                        ({match.result.homeScore45} - {match.result.awayScore45}
+                        )
+                      </div>
+                    </p>
+                    <div className="awayTeam">
+                      <img
+                        className="logo"
+                        src={match.awayTeam?.logo.url}
+                        alt={match.awayTeam?.name}
+                      />
+                      <p>{match.awayTeam?.name}</p>
                     </div>
                   </div>
-                  {match.coveredLive ? (
-                    <div className="liveInfo">
-                      <>
-                        <Tv />
-                        <p>
-                          {match.tvChannels
-                            ?.map((channel) => channel.name)
-                            .join(", ")}
-                        </p>
-                      </>
+                  <hr />
+                  {/* Information about stadium and livefeed */}
+                  <div className="matchInfo">
+                    <div className="stadiumInfo">
+                      <div className="stadium">
+                        <MapPin />
+                        <p>{match.stadium?.name}</p>
+                      </div>
+                      <div className="attendance">
+                        <Users size={24} />
+                        <p>{match.attendance}</p>
+                      </div>
                     </div>
-                  ) : null}
-                </div>
-                <hr />
-                <div className="reporter">
-                  Reporter:{" "}
-                  {match.liveFeeds ? (
-                    <p>{match.liveFeeds?.map((user) => user.user.name)}</p>
-                  ) : null}
+                    {match.coveredLive ? (
+                      <div className="liveInfo">
+                        <>
+                          <Tv />
+                          <p>
+                            {match.tvChannels
+                              ?.map((channel) => channel.name)
+                              .join(", ")}
+                          </p>
+                        </>
+                      </div>
+                    ) : null}
+                  </div>
+                  <hr />
+                  {/* The reporter of the match */}
+                  <div className="reporter">
+                    Reporter:{" "}
+                    {match.liveFeeds ? (
+                      <p>
+                        {match.liveFeeds
+                          ?.map((feed) => feed.user.name)
+                          .join(", ")}
+                      </p>
+                    ) : null}
+                  </div>
                 </div>
               </div>
-            </div>
-          ))
+            );
+          })
         ) : (
           <p></p>
         )}
